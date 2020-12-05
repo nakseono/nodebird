844328e4222a944384a1104f060c253419dc5544
NakSeoNo <nakseono@gmail.com>
2020-12-05 19:57:39 +0900

    더미데이터와 포스트폼 만들기

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import AppLayout from "../components/AppLayout";
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
 
 const Home = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
   return (
     <AppLayout>
-      <div>Hello, World!</div>
+      {isLoggedIn && <PostForm />}
+      {mainPosts.map((id, post) => (
+        <PostCard key={id} post={post} />
+      ))}
     </AppLayout>
   );
 };
