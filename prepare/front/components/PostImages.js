e590740649fb4b78f1ce3bb9e9dc5cdc454f3f1a
NakSeoNo <nakseono@gmail.com>
2020-12-17 22:33:19 +0900

    이미지구현하기 #1

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,5 +1,38 @@
+import React, { useCallback } from "react";
+
 const PostImages = ({ images }) => {
-  return <div>구현중..</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role="presentation"
+          width="50%"
+          src={images[0].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
 };
 
 export default PostImages;
