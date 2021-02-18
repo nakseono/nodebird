4a69611a07e4663f77cd2e5cb89297b59a0c53f6
nakseono <nakseono@gmail.com>
2021-02-18 17:38:40 +0900

    link

--- prepare/review/components/AppLayout.js
+++ prepare/review/components/AppLayout.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 const AppLayout = ({ children }) => {
   // 여기서 children은 AppLayout으로 감싸질 페이지의 내용이다..!
   return (
     <div>
-      <div>공통메뉴</div>
+      <div>
+        <Link href="/">
+          <a>메인페이지</a>
+        </Link>
+        <Link href="/profile">
+          <a>프로필</a>
+        </Link>
+        <Link href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </div>
       {children}
     </div>
   );
