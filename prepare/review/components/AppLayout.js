83494ae322dc249f7c8cd3862a61da8c57ae2487
nakseono <nakseono@gmail.com>
2021-03-08 18:22:00 +0900

    antd와 styled-components

--- prepare/review/components/AppLayout.js
+++ prepare/review/components/AppLayout.js
@@ -1,21 +1,28 @@
 import React from "react";
 import Link from "next/link";
+import { Menu } from "antd";
 
 const AppLayout = ({ children }) => {
   // 여기서 children은 AppLayout으로 감싸질 페이지의 내용이다..!
   return (
     <div>
-      <div>
-        <Link href="/">
-          <a>메인페이지</a>
-        </Link>
-        <Link href="/profile">
-          <a>프로필</a>
-        </Link>
-        <Link href="/signup">
-          <a>회원가입</a>
-        </Link>
-      </div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>메인페이지</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
       {children}
     </div>
   );
