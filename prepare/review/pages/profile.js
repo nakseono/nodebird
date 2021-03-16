756f3ad6cecc140679f04e2abb51f00a47fabda3
nakseono <nakseono@gmail.com>
2021-03-16 19:34:40 +0900

    프로필 페이지 만들기

--- prepare/review/pages/profile.js
+++ prepare/review/pages/profile.js
@@ -1,14 +1,33 @@
 import React from "react";
 import Head from "next/head";
 
+import AppLayout from "../components/AppLayout";
+import FollowList from "../components/FollowList";
+import NicknameEditForm from "../components/NicknameEditForm";
+
 const Profile = () => {
+  const followingList = [
+    { nickname: "심재영" },
+    { nickname: "바보" },
+    { nickname: "못난이" },
+  ];
+  const followerList = [
+    { nickname: "심재영" },
+    { nickname: "바보" },
+    { nickname: "못난이" },
+  ];
+
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <div>내 프로필</div>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
