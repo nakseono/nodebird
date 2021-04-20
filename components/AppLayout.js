210f5bcd618e782e15401a09e572cf453c8a9ae6
nakseono <nakseono@gmail.com>
2021-04-20 16:23:33 +0900

    link와 eslint

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>공통메뉴</div>
+      <Link href="/">
+        <a>노드버드</a>
+      </Link>
+      <Link href="/profile">
+        <a>프로필</a>
+      </Link>
+      <Link href="/signup">
+        <a>회원가입</a>
+      </Link>
       {children}
     </div>
   );
