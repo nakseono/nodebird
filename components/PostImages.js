ef4cdfa7a0b4115c3ee373daf61c5318d83b2357
nakseono <nakseono@gmail.com>
2021-05-04 21:37:32 +0900

    이미지 구현하기

--- components/PostImages.js
+++ components/PostImages.js
@@ -1,5 +1,50 @@
+import React, { useCallback, useState } from "react";
+
 const PostImages = ({ images }) => {
-  return <div>구현 중...</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <>
+        <img src={(images = [0].src)} alt={images[0].src} onClick={onZoom} />
+      </>
+    );
+  }
+
+  if (images.length === 2) {
+    return (
+      <>
+        <img src={(images = [0].src)} alt={images[0].src} onClick={onZoom} />
+        <img src={(images = [1].src)} alt={images[1].src} onClick={onZoom} />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div>
+        <img src={(images = [0].src)} alt={images[0].src} onClick={onZoom} />
+        <div
+          style={{
+            display: "inline-block",
+            width: "50%",
+            textAlign: "center",
+            verticalAlign: "middle",
+          }}
+          onClick={onZoom}
+        >
+          <PlueOutlined />
+          <br />
+          {images.length - 1}
+          개의 사진 더보기
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PostImages;
