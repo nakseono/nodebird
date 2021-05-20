793ac72551d7a06e0227cdb7671da0a3288eae50
nakseono <nakseono@gmail.com>
2021-05-20 19:54:04 +0900

    컴포넌트 폴더 구조 (Code Split)

--- components/ImagesZoom/index.js
+++ components/ImagesZoom/index.js
@@ -1,89 +1,14 @@
 import React, { useState } from "react";
 import Slick from "react-slick";
-import styled, { createGlobalStyle } from "styled-components";
-import { CloseOutlined } from "@ant-design/icons";
-
-const Overlay = styled.div`
-  position: fixed;
-  z-index: 5000;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-
-  & h1 {
-    margin: 0;
-    font-size: 17px;
-    color: #333;
-    line-height: 44px;
-  }
-
-  & button {
-    position: absolute;
-    right: 0;
-    top: 0;
-    padding: 15px;
-    line-height: 14px;
-    cursor: pointer;
-  }
-`;
-
-const Header = styled.header`
-  height: 44px;
-  background: white;
-  position: relative;
-  padding: 0;
-  text-align: center;
-`;
-
-const SlickWrapper = styled.div`
-  height: calc(100% - 44px);
-  background: #090909;
-`;
-
-const ImgWrapper = styled.div`
-  padding: 32px;
-  text-align: center;
-
-  & img {
-    margin: 0 auto;
-    max-height: 750px;
-  }
-`;
-
-const Indicator = styled.div`
-  text-align: center;
-
-  & > div {
-    width: 75px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 15px;
-    background: #313131;
-    display: inline-block;
-    text-align: center;
-    color: white;
-    font-size: 15px;
-  }
-`;
-
-const CloseBtn = styled(CloseOutlined)`
-  position: absolute;
-  right: 0;
-  top: 0;
-  padding: 15px;
-  line-height: 14px;
-  cursor: pointer;
-`;
-
-const Global = createGlobalStyle`
-  .slick-slide {
-    display: inline-block;
-  }
-  .ant-card-cover {
-    transform: none !important;
-  }
-`;
+import {
+  Overlay,
+  Global,
+  Header,
+  CloseBtn,
+  ImgWrapper,
+  Indicator,
+  SlickWrapper,
+} from "./styles";
 
 const ImagesZoom = ({ images, onClose }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -99,7 +24,7 @@ const ImagesZoom = ({ images, onClose }) => {
         <div>
           <Slick
             initialSlide={0}
-            afterChange={(slide) => setCurrentSlide(slide)}
+            beforeChange={(slide) => setCurrentSlide(slide)}
             infinite
             arrows={false}
             slidesToShow={1}
