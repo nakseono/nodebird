2db5f95055d5a3d17c7fa3227083690b925e596a
nakseono <nakseono@gmail.com>
2022-05-09 18:40:47 +0900

    초기화

--- README.md
+++
@@ -1,1 +0,0 @@
-인프런 NodeBird(React) 강좌를 듣고 공부한 것을 정리한 Repo.
--- sagas/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { all, call, fork, put, takeLatest, delay } from "redux-saga/effects";
-
-import postSaga from "./post";
-import userSaga from "./user";
-
-export default function* rootSaga() {
-  yield all([fork(postSaga), fork(userSaga)]);
-}
