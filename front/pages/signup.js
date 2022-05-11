dc3f62cb30610b054f073174e042258c4689a47f
nakseono <nakseono@gmail.com>
2022-05-11 23:49:34 +0900

    _app.js와 Head

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Head from "next/head";
 
 import AppLayout from "../components/AppLayout";
 
 const SignUp = () => {
   return (
-    <AppLayout>
-      <div>회원가입 페이지</div>
-    </AppLayout>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <div>회원가입 페이지</div>
+      </AppLayout>
+    </>
   );
 };
 
