e5d931d1fee12c5db670c9d7eaeb03f1a10a4da9
nakseono <nakseono@gmail.com>
2022-06-01 16:12:39 +0900

    더미데이터와 포스트폼

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,9 +1,66 @@
 export const initialState = {
-  mainPosts: [],
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "서노",
+      },
+      content: "첫 번째 게시글 #해시태그 #익스프레스",
+      Images: [
+        {
+          src: "https://i.ibb.co/qW3395P/26.gif",
+        },
+        {
+          src: "https://i.ibb.co/v4GQkWQ/28.png",
+        },
+        {
+          src: "https://i.ibb.co/k9wGr0M/20.png",
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: "dlwlrma",
+          },
+          content: "Love Poem",
+        },
+        {
+          User: {
+            nickname: "이지금",
+          },
+          content: "이름에게",
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
+
+const ADD_POST = "ADD_POST";
+export const addPost = {
+  type: ADD_POST,
+};
+export const dummyPost = {
+  id: 2,
+  content: "더미데이터 입니다.",
+  User: {
+    id: 1,
+    nickname: "이지금",
+  },
+  Images: [],
+  Comments: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
     default:
       return state;
   }
