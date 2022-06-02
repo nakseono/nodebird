60a8b9772b587ebdb04826dfe01abdd7c930bb9f
nakseono <nakseono@gmail.com>
2022-06-02 15:24:37 +0900

    remove thunk & install saga

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,12 +1,14 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import { createWrapper } from "next-redux-wrapper";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunkMiddleWare from "redux-thunk";
+import createSagaMiddleWare from "redux-saga";
 
 import reducer from "../reducers";
+import rootSaga from "../sagas";
 
 const configureStore = () => {
-  const middlewares = [thunkMiddleWare];
+  const sagaMiddleWare = createSagaMiddleWare();
+  const middlewares = [sagaMiddleWare];
 
   const enhancer =
     process.env.NODE_ENV === "production"
@@ -14,6 +16,7 @@ const configureStore = () => {
       : composeWithDevTools(applyMiddleware(...middlewares));
 
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleWare.run(rootSaga);
   return store;
 };
 
