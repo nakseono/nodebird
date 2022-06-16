63b9fdb44df9ae8b2086de66d51b12e3adec5a30
nakseono <nakseono@gmail.com>
2022-06-16 17:22:14 +0900

    시퀄라이즈 모델 만들기

---
+++ back/models/image.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize/types");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+  const Image = sequelize.define(
+    //! sequelize.difine 이후에 나오는 첫 인수에 넣은 'Image' 는 mysql에 자동으로 'Images' 테이블로 생성 및 저장된다.
+    "Image",
+    {
+      //! 두번째 인수는 방금 생성된 테이블의 셀을 만들어주는 것이고 (어떤게 들어있을지)
+      // 나중에 활용하게 되는 id는 mysql에서 자동으로 만들어준다.
+      src: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+      },
+    },
+    {
+      //! 세번째 인수는 두번째 인수에서 만든 테이블의 설정을 한다.
+      charset: "utf8",
+      collate: "utf8_general_ci", // 한글 저장
+    }
+  );
+  Image.associate = (db) => {};
+  return Image;
+};
