62b6eaa89b158b6d6330967b305a7d23d0c49966
nakseono <nakseono@gmail.com>
2022-08-25 14:12:55 +0900

    router 분리

---
+++ back/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const postRouter = require("./routes/post");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
+
+app.get("/api/posts", (req, res) => {
+  res.json([
+    { id: 1, content: "hello" },
+    { id: 2, content: "hello" },
+    { id: 3, content: "hello" },
+  ]);
+});
+
+app.use("/post", postRouter); // route 분기해주면서 prefix 붙은 것.
+
+app.listen(3065, () => {
+  console.log("서버 실행 중");
+});
